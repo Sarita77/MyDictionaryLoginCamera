5cb3e503522a7444879c26baa0487f3758c9720a
Sarita Asthani <asthani77@outlook.com>
2019-05-01 13:45:12 +0545

    Login Register functionality is added along with camera app to the previous capital-country app

---
+++ app/src/main/java/com/yuskay/mydictionary/DashboardActivity.java
@@ -0,0 +1,18 @@
+package com.yuskay.mydictionary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class DashboardActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+
+    }
+}
